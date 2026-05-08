2869255deab2b9cb2c7e982b0aed0cd8cc94cdb4
sumaiya suchi <sumaiyamoina@gmail.com>
2026-05-08 16:09:35 +0600

    adding the third problem solution

--- solutions.ts
+++ solutions.ts
@@ -1,8 +1,27 @@
+//Problem - 1
 const filterEvenNumbers =(array : number[]) : number[]=>{
     return array.filter (num => num%2 ===0)
 }
 
+//Problem - 2
 
 const reverseString=(str : string) : string =>{
     return str.split('').reverse().join('')
 }
+
+//Problem - 3
+
+type StringOrNumber = string | number;
+
+const checkType=(type : StringOrNumber ) : string =>
+{
+    if( typeof type === "string")
+    {
+        return "String";
+    }
+    else 
+    {
+        return "Number";
+    }
+}
+
