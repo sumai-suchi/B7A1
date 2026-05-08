d3e64178ef3e0c1b32abf32eb93bfe36e6b2af32
sumaiya suchi <sumaiyamoina@gmail.com>
2026-05-08 17:52:15 +0600

    added the 7th number problem

--- solutions.ts
+++ solutions.ts
@@ -46,3 +46,33 @@ const toggleReadStatus =(book : Book) : Book & {isRead : boolean} => {
     }
 }
 
+//Problem - 6
+class Person {
+    name :string ;
+    age :number ;
+    constructor(name : string , age :number ){
+        this.name =name;
+        this.age= age;
+    }
+}
+
+class Student extends Person {
+      
+    grade : string
+    constructor( name :string , age :number ,  grade :string)
+    {
+         super(name , age)
+         this.grade = grade;
+    }
+
+    getDetails (): string{  
+        return  `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}` 
+    }
+       
+}
+
+//Problem -7
+
+const getIntersection = (array1 : number[] , array2 : number[]) : number[] => {
+    return array1.filter(item => array2.includes(item))
+}
